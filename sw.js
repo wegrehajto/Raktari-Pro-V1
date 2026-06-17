06b245d4907529d52c7ce01648b2fa14d776efcf
wegrehajto <p8nfj5sc7j@privaterelay.appleid.com>
2026-06-17 20:03:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
-const CACHE_NAME = "raktar-v1";
+const CACHE_NAME = "raktar-pro-v1";
 
-const urlsToCache = [
+const urls = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urls))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
